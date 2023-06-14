d992403c9b5dd579db9e1ec97a30f057f39003ff
Diegoc24 <carusodiegod@gmail.com>
2023-06-14 00:17:22 -0300

    feature/backend(added_file)

--- database/config/relationships.js
+++ database/config/relationships.js
@@ -1,7 +1,15 @@
 const Reviews = require("../models/reviews.model")
 const User = require("../models/user.model")
+const Post = require("../models/posts.model")
+
+User.hasMany(Reviews, { foreignKey: "userId" })
+Reviews.belongsTo(User, {foreignKey: "userId"})
+
+Reviews.hasMany(Post, {foreignKey: "reviewsId"})
+Post.belongsTo(Reviews, {foreignKey: "reviewsId"})
 
 module.exports = {
     Reviews,
-    User
+    User,
+    Post
 }
